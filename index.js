f83ab0cc86573929900f54f553568c9a80a4a14e
TehronNaimov <tehron.6576@gmail.com>
2025-01-24 10:43:21 +0500

    masalalar toplami

---
+++ index.js
@@ -0,0 +1,403 @@
+// Types
+// 1-masala
+// let son1 = -7;
+// let son2 = 10;
+// console.log(son1 + son2);
+
+// 2-masala
+// let cm = 100;
+// console.log(cm / 100);
+
+// 3-masala
+// let kg = 2000;
+// console.log(kg / 1000);
+
+// 4-masala
+// let a = 5;
+// let b = 9;
+// let c = 7;
+// console.log((a + b + c) / 3);
+
+// 5-masala
+// let yosh = 65;
+// console.log(yosh * 365);
+
+// 6-masala
+// let hour = 3;
+// console.log(hour * 60 * 60);
+
+// 7-masala
+// let tovuq = 2;
+// let sigir = 4;
+// let chochqa = 5;
+// console.log(tovuq * 2 + sigir * 4 + chochqa * 4);
+
+// 8-masala
+// a = 4;
+// b = 5;
+// c = 0;
+// c = a;
+// a = b;
+// b = c;
+// console.log(a,b);
+
+// 9-masala
+// a = 32;
+// let birlik = a % 10;
+// let onlik = (a - birlik) / 10;
+// console.log(birlik + onlik);
+
+// 10-masala
+// a = 26;
+// birlik = a % 10;
+// onlik = (a - birlik) / 10;
+// c = 0;
+// c = birlik;
+// birlik = onlik;
+// onlik = c;
+// console.log(birlik , onlik);
+
+// 11-masala
+// let a = 777;
+// let onlik = a % 100;
+// let birlik = onlik % 10;
+// let yuzlik = (a - onlik) / 100;
+// console.log(yuzlik);
+
+// 12-masala
+// let a = 555;
+// let onlik = a % 100;
+// let birlik = onlik % 10;
+// let yuzlik = (a - onlik1) / 100;
+// let onlik2 = (onlik1 - birlik) / 10;
+// console.log(yuzlik + onlik2+ birlik);
+
+// 13-masala
+// let a = 434;
+// let onlik34 = a % 100;
+// let birlik = onlik34 % 10;
+// let onlik3 = (onlik34 - birlik) / 10;
+// let yuzlik = (a - onlik34) / 100;
+// let b = 0;
+// birlik = yuzlik;
+// yuzlik = b;
+// console.log(yuzlik,onlik3,birlik);
+
+// 14-masala
+// (Kirish=123;Natija=213).
+// ishlay olmadim
+
+// 15-masala
+// chala
+
+//  Operators
+
+// 1-masala
+// let a = 2;
+// console.log(a > 0);
+
+// 2-masala
+// let a = 5;
+// console.log(a % 2 == 1);
+
+// 3-masala
+// let a = 6;
+// console.log(a % 2 == 0);
+
+// 4-masala
+// let a = 2;
+// let b = 3;
+// console.log(a > 2 && b < 3);
+
+// 5-masala
+// let a = 0;
+// let b = -2;
+// console.log(a >= 0) &&(b<-2);
+
+// 6-masala
+// let a = 6;
+// let b = 8;
+// let c = 10;
+// console.log(a > b && b < c);
+
+// 7-masala
+// let a = 5;
+// let b = 7;
+// console.log(a % 2 == 1 && b % 2 == 1);
+
+// 8-masala
+// let a = 6;
+// let b = 9;
+// console.log(a % 2 == 1 || b % 2 == 1);
+
+// 9-masala
+// let a = 4;
+// let b = 9;
+// console.log((a % 2 == 1 && b % 2 == 1) || (a % 2 == 0 && b % 2 == 0));
+
+// 10-masala
+// let a = 3;
+// let b = 5;
+// let c = 7;
+// console.log(a > 0 && b > 0 && c > 0);
+
+// 11-masala
+// let a = 3;
+// let b = 5;
+// let c = 7;
+// console.log(a > 0 || b > 0 || c > 0);
+
+// 12-masala
+// let a = 6;
+// let b = -3;
+// console.log(a > 0 || b > 0);
+
+// If else
+
+// 1-masala
+// let a = 5;
+// if (a % 2 == 1) {
+//   console.log(a+"- son juft son");
+// } else {
+//   console.log(a+"- son toq son");
+// }
+
+// 2-masala
+// let a = 6;
+// let b = 4;
+// if (a > b) {
+//   console.log(a - b);
+// } else {
+//   console.log(b - a);
+// }
+
+// 3-masala
+// let a = 15;
+// if ((a / 3 == 5 && a / 5 == 3) || a > 0) {
+//   console.log(a ** 2);
+// } else {
+//   console.log(a ** 3);
+// }
+
+// 4-masala
+// let a = 8;
+// if (a > 0) {
+//   let res = ++a;
+//   console.log(res);
+// } else {
+//   console.log(a);
+// }
+
+// 5-masala
+// let = 9;
+// if (a > 0) {
+//   let res = ++a;
+//   console.log(res);
+// } else {
+//   let b = a - 2;
+//   console.log(b);
+// }
+
+// 6-masala
+// let a = 5;
+// if (a > 0) {
+//   let res = ++a;
+//   console.log(res);
+// } else if (a == 0) {
+//   console.log((a = 10));
+// } else {
+//   let b = a - 2;
+//   console.log(b);
+// }
+
+// 7-masala
+// let a = 8;
+// let b = 5;
+// let c = 2;
+// if ((a % 2 == 0, b % 2 == 1, c % 2 == 0)) {
+//   console.log("3 ta son ham musbat");
+// } else {
+//   console.log("1ta son musbat son");
+// }
+// 8-masala
+// let a = 6;
+// let b = 4;
+// if (a > b) {
+//   console.log(a + "-soni katta son");
+// } else {
+//   console.log(b + "-soni katta son");
+// }
+
+// 9-masala
+// let a = 4;
+// let b = 4;
+// if (a != b) {
+//   console.log(a + b);
+// } else {
+//   console.log(0);
+// }
+
+// 10-masala
+// let a = 11;
+// let b = 7;
+// let c = 5;
+// if (a > b && a > c) {
+//   console.log(b + "-soni ortachasi");
+// } else {
+//   console.log(a - c);
+// } tekshirish k.k
+
+// 11-masala
+// let num = 7;
+
+// switch (num) {
+//   case 1:
+//     console.log("DUSHANBA");
+//     break;
+//   case 2:
+//     console.log("seshanba");
+//     break;
+//   case 3:
+//     console.log("chorshanba");
+//     break;
+//   case 4:
+//     console.log("payshanba");
+//     break;
+//   case 5:
+//     console.log("juma");
+//     break;
+//   case 6:
+//     console.log("shanba");
+//     break;
+//   case 7:
+//     console.log("yakshanba");
+//     break;
+// }
+
+// 12-masala
+// let num = 6;
+
+// switch (num) {
+//   case 1:
+//     console.log("yomon");
+//     break;
+//   case 2:
+//     console.log("qoniqarsiz");
+//     break;
+//   case 3:
+//     console.log("qoniqarli");
+//     break;
+//   case 4:
+//     console.log("yaxshi");
+//     break;
+//   case 5:
+//     console.log("a'lo");
+//     break;
+//   default:
+//     console.log("xato");
+// }
+
+// 13-masala
+// let num = 2;
+// switch (num) {
+//   case 1:
+//     console.log(a+"-yanvar qish");
+//     break;
+//   case 2:
+//     console.log(a+"-fevral qish");
+//     break;
+//   case 3:
+//     console.log(a+"-mart bahor");
+//     break;
+//   case 4:
+//     console.log(a+"-aprel bahor");
+//     break;
+//   case 5:
+//     console.log(a+"-may bahor");
+//     break;
+//   case 6:
+//     console.log(a+"-iyun yoz");
+//     break;
+//   case 7:
+//     console.log(a+"-iyul yoz");
+//     break;
+//   case 8:
+//     console.log(a+"-avgust yoz");
+//     break;
+//   case 9:
+//     console.log(a+"-sentabr kuz");
+//     break;
+//   case 10:
+//     console.log(a+"-oktabr kuz");
+//     break;
+//   case 11:
+//     console.log(a+"-noyabr kuz");
+//     break;
+//   case 12:
+//     console.log(a+"-dekabr qish");
+//     break;
+// }
+
+// Loops
+// 1-masala
+// for( let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// 2-masala
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// 3-masala
+// let n = 5;
+// let k = 10;
+// for (n = 5; n > 0; i++) {
+//   console.log(n);
+// }
+
+// 4-masala
+// for ( let i = 1; i <= 30; i += 2) {
+//   console.log(i);
+// }
+
+// 5-masala
+// let res = 0;
+// for (let i = 0; i <= 10; i++) {
+//   res += i;
+// }
+// console.log(res);
+
+// 6-masala
+// let a = 21;
+// let res = 0;
+// for (let i = 0; i <= 10; i ++) {
+// if (i % 2 == 0)
+//      res += i;
+//   console.log(i);
+// }
+
+// 7-masala
+// let x = 2;
+// let y = 36;
+// for (let i = x; i <= y; i++) {
+//   if (i % 5 == 0) console.log(i);
+// }
+
+// 8-masala
+// let kg = 25000;
+// for (let i = kg; i <= 10; i++) {
+//   console.log(kg * i);
+// }
+
+// 9-masala
+// for (let i = 4444; i <= 9999; i++) {
+//   if (i % 47 == 43)
+//      console.log(i);
+// }
+
+// 10-masala
+// let n = 34;
+// for (let i = 1; i <= n; i += 2) {
+//   console.log(i);
+// }
